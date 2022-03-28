999561d75634aeceaeef5806cc68aa831589fb55
hugosenari <hugosenari@gmail.com>
2022-03-27 21:49:22 -0300

    chore: re index eth_bloom

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-eth_bloom-master.ref   = "refs/heads/master";
   inputs.src-eth_bloom-master.owner = "status-im";
   inputs.src-eth_bloom-master.repo  = "nim-eth-bloom";
-  inputs.src-eth_bloom-master.dir   = "";
   inputs.src-eth_bloom-master.type  = "github";
   
   inputs."nimcrypto".owner = "nim-nix-pkgs";
