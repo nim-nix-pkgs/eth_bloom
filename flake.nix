c6fbd165d4d601e5a75f7126c1fd9b3063282389
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:21 -0300

    chore: re index eth_bloom

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."eth_bloom-master".dir   = "master";
+  inputs."eth_bloom-master".owner = "nim-nix-pkgs";
+  inputs."eth_bloom-master".ref   = "master";
+  inputs."eth_bloom-master".repo  = "eth_bloom";
+  inputs."eth_bloom-master".type  = "github";
+  inputs."eth_bloom-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."eth_bloom-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
